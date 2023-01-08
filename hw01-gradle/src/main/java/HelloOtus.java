ba15c984682e65f84d9f5cbb68c058f919fd3d10
boldrqwe <70112965+boldrqwe@users.noreply.github.com>
2023-01-08 17:17:41 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -0,0 +1,11 @@
+import java.util.StringJoiner;
+
+import com.google.common.base.Joiner;
+
+public class HelloOtus {
+    public static void main(String... args) {
+
+        Joiner on = Joiner.on(" ");
+        System.out.println(on.join("O", "T", "U", "S"));
+    }
+}
