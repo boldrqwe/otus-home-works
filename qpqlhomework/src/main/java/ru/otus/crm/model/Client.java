7a689e74887ecbb64a8bb89e6ffd1c4e68a0e031
boldrqwe <70112965+boldrqwe@users.noreply.github.com>
2023-06-03 19:40:30 +0300

    jpql

---
+++ qpqlhomework/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,80 @@
+package ru.otus.crm.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "client_SEQ", sequenceName = "client_SEQ", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phones;
+
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        address.setClient(this);
+        phones.forEach(p-> p.setClient(this));
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id,this.name,this.address,this.phones);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
